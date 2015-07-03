bffd8cd1c76f41a82022b3dea2e43e422a1d2d04
inokappa <inokara@gmail.com>
2015-07-03 12:00:12 +0000

    add consul

--- sample-prj-20150305/inventories/docker_inventory.rb
+++ sample-prj-20150305/inventories/docker_inventory.rb
@@ -3,8 +3,8 @@
 require 'docker'
 require 'json'
 
-docker_host = "192.168.59.103" # boot2docker 
-target_host_name = "ansible_"
+docker_host = "127.0.0.1" # localhost docker
+target_host_name = "monitoring"
 target_user_name = "ansible"
 target_user_pass = "ansible"
 
@@ -17,7 +17,9 @@ target = {}
 
 targets.map do |t|
   t.info['Ports'].each do |p|
-    target.merge!({ t.info['Names'][0].gsub("/","") => { "ansible_ssh_host" => docker_host, "ansible_ssh_port" =>  p['PublicPort'], "ansible_ssh_user" => target_user_name, "ansible_ssh_pass" => target_user_pass }})
+    if p['PrivatePort']  == 22
+      target.merge!({ t.info['Names'][0].gsub("/","") => { "ansible_ssh_host" => docker_host, "ansible_ssh_port" =>  p['PublicPort'], "ansible_ssh_user" => target_user_name, "ansible_ssh_pass" => target_user_pass }})
+    end
   end
 end
 
